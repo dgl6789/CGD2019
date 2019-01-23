1756e186f24d0ffec1a7ffd0faaacd30d3a65358
PGeaslinRIT <pxg6350@rit.edu>
2019-01-23 16:40:53 -0500

    Gem Spawning Only in Rock
    Gems spawned outside of rock are pulled in to the origin. Currenlty still results in floating gems if the origin is spawned with no rock around it

--- CGD2019.P1.MiningGame/Assets/scripts/Gemeration.cs
+++ CGD2019.P1.MiningGame/Assets/scripts/Gemeration.cs
@@ -1,87 +1,172 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using App.Gameplay;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Gemeration : MonoBehaviour {
-
-    //gemeration values
-    public int gemCount = 0;
-    public int range = 0;
-    public List<GameObject> gemPrefabs = new List<GameObject>();
-
-    //gemerator position
-    Vector3 gemOrigin;
-
-	// Use this for initialization
-	void Start () {
-        gemOrigin = gameObject.transform.position;
-
-        GenerateGems();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    //generate a number of gems
-    public void GenerateGems()
+namespace App
+{
+    public class Gemeration : MonoBehaviour
     {
-        //loop to generate gems
-        for (int i = 0; i < gemCount; i++)
-        {
-            //randomly choose a prefab
-            GameObject thisGem = Instantiate(ChoosePrefab());
+        /// Reference to the rock's voxel grid.
+        [SerializeField] VoxelGrid voxelGrid;
 
-            //if there are no prefabs, exit
-            if (thisGem == null)
+        //gemeration values
+        public int gemCount = 1;
+        public int range = 0;
+        public List<GameObject> gemPrefabs = new List<GameObject>();
+
+        //gemerator position
+        Vector3 gemOrigin;
+
+        // Use this for initialization
+        void Start()
+        {
+            gemOrigin = FindOrigin();
+
+            if (range <= 0)
             {
-                Debug.Log("ERROR: No gem prefabs found during gemeration process\n");
-                return;
+                range = FindDefaultRange();
             }
 
-            //generate random position and normalize it
-            Vector3 gemPosition = new Vector3(
-                Random.Range(-1.0f, 1.0f),
-                Random.Range(-1.0f, 1.0f),
-                Random.Range(-1.0f, 1.0f)).normalized;
+            if (gemCount <= 0)
+                gemCount = 1;
 
-            //place position within specified range of origin
-            float rangePercentage = Random.Range(0.0f, 1.0f) * range;
-            gemPosition = gemPosition * rangePercentage + gemOrigin;
-
-            Debug.Log("Placing gem " + i + " at " + gemPosition);
-
-            //set the gem's position
-            thisGem.transform.position = gemPosition;
-            Debug.Log("this gem's position = " + thisGem.transform.position + "\n");
+            GenerateGems();
         }
-    }
 
-    //choose a gem prefab to use
-    private GameObject ChoosePrefab()
-    {
-        //null if there are no prefabs set
-        if (gemPrefabs.Count == 0)
+        // Update is called once per frame
+        void Update()
         {
-            return null;
+
         }
 
-        GameObject thisGem;
-
-        //if there's only one prefab, choose that one
-        if (gemPrefabs.Count == 1)
+        //generate a number of gems
+        public void GenerateGems()
         {
-            thisGem = gemPrefabs[0];
+            //loop to generate gems
+            for (int i = 0; i < gemCount; i++)
+            {
+                //randomly choose a prefab
+                GameObject thisGem = Instantiate(ChoosePrefab());
+
+                //if there are no prefabs, exit
+                if (thisGem == null)
+                {
+                    Debug.Log("ERROR: No gem prefabs found during gemeration process\n");
+                    return;
+                }
+
+                //generate random position and normalize it
+                Vector3 gemPosition = new Vector3(
+                    Random.Range(-1.0f, 1.0f),
+                    Random.Range(-1.0f, 1.0f),
+                    Random.Range(-1.0f, 1.0f)).normalized;
+
+                //place position within specified range of origin
+                float rangePercentage = Random.Range(0.0f, 1.0f) * range;
+                gemPosition = gemPosition * rangePercentage + gemOrigin;
+
+                //correct for floating gems
+                gemPosition = PullToRock(gemPosition);
+
+                //set the gem's position
+                thisGem.transform.position = gemPosition;
+            }
         }
-        else
+
+        //choose a gem prefab to use
+        private GameObject ChoosePrefab()
         {
-            //choose a prefab at random if there are prefabs to choose from
-            int chosenPrefab = Random.Range(0, gemPrefabs.Count);
+            //null if there are no prefabs set
+            if (gemPrefabs.Count == 0)
+            {
+                return null;
+            }
 
-            thisGem = gemPrefabs[chosenPrefab];
+            GameObject thisGem;
+
+            //if there's only one prefab, choose that one
+            if (gemPrefabs.Count == 1)
+            {
+                thisGem = gemPrefabs[0];
+            }
+            else
+            {
+                //choose a prefab at random if there are prefabs to choose from
+                int chosenPrefab = Random.Range(0, gemPrefabs.Count);
+
+                thisGem = gemPrefabs[chosenPrefab];
+            }
+
+            return thisGem;
         }
 
-        return thisGem;
+        //method to find the center of the voxel grid
+        Vector3 FindOrigin()
+        {
+            return new Vector3(voxelGrid.X / 2.0f, voxelGrid.Y / 2.0f, voxelGrid.Z / 2.0f);
+        }
+
+        //method to find default range value using voxel grid dimensions
+        int FindDefaultRange()
+        {
+            return (int)((voxelGrid.X + voxelGrid.Y + voxelGrid.Z) / 3.0f);
+        }
+
+        //method to correct for floating gems
+        Vector3 PullToRock(Vector3 gemPosition, bool firstPass = true)
+        {
+            //return if gem is in rock
+            if (InRock(gemPosition))
+            {
+                Debug.Log("Gem is fine\n");
+                return gemPosition;
+            }
+            else if (firstPass) //correct for position outside of rock grid
+            {
+                if (gemPosition.x > voxelGrid.X)
+                    gemPosition.x = voxelGrid.X;
+                else if (gemPosition.x < 0)
+                    gemPosition.x = 0;
+
+                if (gemPosition.y > voxelGrid.Y)
+                    gemPosition.y = voxelGrid.Y;
+                else if (gemPosition.y < 0)
+                    gemPosition.y = 0;
+
+                if (gemPosition.z > voxelGrid.Z)
+                    gemPosition.z = voxelGrid.Z;
+                else if (gemPosition.z < 0)
+                    gemPosition.z = 0;
+
+                //Debug.Log(gemPosition + " is within voxelgrid bounds of " + voxelGrid.X + "," + voxelGrid.Y + "," + voxelGrid.Z + "\n");
+
+                return PullToRock(gemPosition, false);
+            }
+            else if (Mathf.Abs(gemPosition.x - gemOrigin.x) <= 0.5f && Mathf.Abs(gemPosition.y - gemOrigin.y) <= 0.5f && Mathf.Abs(gemPosition.z - gemOrigin.z) <= 0.5f) //check for close enough to the origin to be safe
+            {
+                return gemPosition;
+            }
+            else //gradually pull gem in closer if it's not in the rock
+            {
+                Vector3 newPosition = gemPosition - gemOrigin;
+                newPosition = newPosition * 0.75f + gemOrigin;
+
+                return PullToRock(newPosition, false);
+            }
+        }
+
+        //method to determine if gem is embedded in rock
+        bool InRock(Vector3 gemPosition)
+        {
+            //if origin is in grid then gem is embedded
+            if (voxelGrid.GetData((int)gemPosition.x, (int)gemPosition.y, (int)gemPosition.z) == 1)
+            {
+                Debug.Log("gem at " + gemPosition + " is in the rock");
+                return true;
+            }
+
+            Debug.Log("gem at " + gemPosition + " is NOT in the rock");
+            return false;
+        }
     }
 }
