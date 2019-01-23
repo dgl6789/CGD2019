532d2d1ae4555720ee9b50aa2ce1f2848b1cd9dd
PGeaslinRIT <pxg6350@rit.edu>
2019-01-23 09:36:39 -0500

    Basic Gem Mining
    Gems can be mined if free of voxel. Stub method to determine mineability
    Basic health/durability based on clicking if still in rock

---
+++ CGD2019.P1.MiningGame/Assets/GemBehavior.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemBehavior : MonoBehaviour
+{
+
+    //voxel mesh reference
+
+    //inventory reference
+
+    //gem behavior values
+    int health = 0;
+
+    public bool debugIsFree = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        health = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //destroy the gem if it's taken too much damage
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //triggers when clicked
+    private void OnMouseDown()
+    {
+        //if the gem is sufficiently clear, mine it
+        if (IsMineable())
+        {
+            Mine();
+        }
+        else
+        {
+            health--;
+        }
+    }
+
+    //method to mine gem
+    void Mine()
+    {
+        //add points and gem to inventory
+
+        //delete gameobject
+        Destroy(gameObject);
+    }
+
+    //method to determine if the gem can be mined
+    bool IsMineable()
+    {
+        return debugIsFree;
+    }
+
+}
