0edb603faff1163150000e9f1453d8fb95b33173
Chris Commisso (RIT Student) <cfc5762@ad.rit.edu>
2019-02-15 09:50:45 -0500

    start

---
+++ CGD2019.P2.CityGame/Assets/Scripts/Draggable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour {
+    Vector3 originalLScale;
+    float mult;
+	// Use this for initialization
+	void Start () {
+        originalLScale = transform.localScale;
+        mult = 0;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+            transform.localScale = originalLScale*mult;
+        
+	}
+    private void OnMouseOver()
+    {
+        transform.localScale=
+    }
+    private void OnMouseExit()
+    {
+        
+    }
+}
