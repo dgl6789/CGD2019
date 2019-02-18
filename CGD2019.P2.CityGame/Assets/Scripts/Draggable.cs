00bc0a37a55ab076c60d74a52b395a0eca9d1cbd
Chris Commisso <qwertykeebored@live.com>
2019-02-18 16:25:49 -0500

    merge conf res

--- CGD2019.P2.CityGame/Assets/Scripts/Draggable.cs
+++ CGD2019.P2.CityGame/Assets/Scripts/Draggable.cs
@@ -3,29 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Draggable : MonoBehaviour {
-<<<<<<< HEAD
-    Vector3 originalLScale;
-    float mult;
-	// Use this for initialization
-	void Start () {
-        originalLScale = transform.localScale;
-        mult = 0;
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        
-            transform.localScale = originalLScale*mult;
-        
-	}
-    private void OnMouseOver()
-    {
-        transform.localScale=
-    }
-    private void OnMouseExit()
-    {
-        
-=======
     public SpriteRenderer renderer;
     Vector3 originalLscale;
     int phase = 0;
@@ -62,6 +39,6 @@ public class Draggable : MonoBehaviour {
             renderer.color = Color.white;
         }
         transform.localScale = originalLscale;
->>>>>>> master
+
     }
 }
